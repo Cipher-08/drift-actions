207366be610f43106cb0223444b53186472cf5f7
Cipher-08 <sakshamawasthi1111@gmail.com>
2025-03-05 18:51:47 +0530

    added something

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "firefly_bucket" {
+resource "aws_s3_bucket" "firefly_9834983bucket-49340893894" {
   bucket = "firefly-drift-detection-bucket"
 }
 
